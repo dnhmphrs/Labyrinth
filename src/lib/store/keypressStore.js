fd7e414bc04742f76370e8904774809b60803f9c
dnhmphrs <45633295+dnhmphrs@users.noreply.github.com>
2025-02-10 14:06:40 +0000

    edit

--- src/lib/store/keypressStore.js
+++ src/lib/store/keypressStore.js
@@ -3,9 +3,6 @@ import { writable } from 'svelte/store';
 export const keypressHistory = writable([]);
 
 export function addKeypress(key) {
-    keypressHistory.update(history => {
-        const timestamp = performance.now();
-        // return [...history, { key, timestamp }].slice(-10);
-        return [...history, { key, timestamp }];
-    });
+    const timestamp = performance.now();
+    keypressHistory.update(history => [{key, timestamp}, ...history]);
 } 
